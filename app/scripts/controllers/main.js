2c9034d53c4e568baf1b129f0f47b5a15147ed25
Chris Blackburn <burnicus8@gmail.com>
2015-08-20 20:04:37 -0400

    move data parsing out of the controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,9 @@
  * Controller of the gameFlowApp
  */
  angular.module('gameFlowApp')
- 	.controller('MainCtrl', ['$scope', 'GameData', 'GameDataParser', function($scope, GameData, GameDataParser) {
- 		GameData.get().then(function(response) {
- 			$scope.gameData = GameDataParser.parse(response.data);
- 		});
+ 	.controller('MainCtrl', ['$scope', 'GameData', function($scope, GameData) {
+ 	  GameData.get().then(function(response) {
+ 	 		$scope.gameData = response;
+ 	 	});
  	}
  ]);
--- app/scripts/services/gameData.js
+++ app/scripts/services/gameData.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('gameFlowApp')
-	.factory('GameData', ['$http', function($http) {
+	.factory('GameData', ['$http', 'GameDataParser', function($http, GameDataParser) {
 		return {
 			get: function() {
-				return $http.get('games/game2.json');
+				return $http.get('games/game2.json').then(function(response) {
+					return GameDataParser.parse(response.data);
+				});
 			}
 		};
 	}]);
