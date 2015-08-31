df927cc64a20e6b6389ff7e48659f63dbf08b7e9
Chris Blackburn <burnicus8@gmail.com>
2015-08-30 22:40:05 -0400

    implement visual cue on mouseover

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,9 +12,31 @@
  	  GameData.get().then(function(response) {
  	 		$scope.gameData = response;
  	 	});
-    $scope.playDetails = {homeScore: 115, awayScore: 120};
-    $scope.setPlayDetails = function(play) {
+    $scope.playDetails = {
+      final: true,
+      homeScore: 115,
+      awayScore: 120,
+      description: 'Hover the game event markers below for play-by-play details'
+    };
+    $scope.setPlayDetails = function(play, $event) {
+      var element = $event.target;
+      var currentTransform = element.getAttribute('transform');
+      if(currentTransform !== null) {
+        var newTransform = currentTransform + 'translate(0, 30)';
+        element.setAttribute('transform', newTransform);
+      }
+      element.setAttribute('stroke-width', 6);
+      $scope.playDetails.final = false;
       $scope.playDetails = play;
+    };
+    $scope.resetMarker = function($event) {
+      var element = $event.target;
+      var currentTransform = element.getAttribute('transform');
+      if(currentTransform !== null) {
+        var newTransform = currentTransform.replace(/translate\(.*\)/, '');
+        element.setAttribute('transform', newTransform);
+      }
+      element.setAttribute('stroke-width', 1);
     };
  	}
  ]);
